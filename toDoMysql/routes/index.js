9469882d9abda30d31a9cfc32d55e7edd82e384c
Katie Duane <katiejduane@gmail.com>
2019-01-24 14:32:34 -0500

    todo app

--- toDoMysql/routes/index.js
+++ toDoMysql/routes/index.js
@@ -8,7 +8,35 @@ connection.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const selectQuery = 'SELECT * FROM tasks;';
+  connection.query(selectQuery,(error, results)=>{
+    res.render('index', {taskArray: results})
+  })
 });
+
+router.post('/addItem',(req, res, next) => {
+  const newTask = req.body.newTask;
+  const newTaskDate = req.body.newTaskDate;
+  // We know what the user submitted in the form. It comes to this route
+  // inside req.body.nameoffield. !e store those values into a var.
+  // Now we take those varsand insertthem into MySql
+
+  // // THIS IS BAD!!!!!! (ie opens up DB to SQL injection, so unsafe)
+  // const insertQuery = `INSERT INTO tasks (taskName, taskDate)
+  //   VALUES
+  //   (${newTask}, ${newTaskDate});`
+  const insertQuery = `INSERT INTO tasks(taskName, taskDate)
+    VALUES
+    (?, ?);`
+  connection.query(insertQuery, [newTask, newTaskDate], (err, results)=>{
+    if(err){
+      throw err;
+    }else{
+      console.log(insertQuery)
+      res.redirect('/')
+    }
+  })
+  // res.json(req.body)
+})
 
 module.exports = router;
